a65157a36a2ad3c8e872c1afb63376312cccb38d
Willinton Ferney Mora Pérez <willintonmora14@gmail.com>
2024-11-26 13:35:03 -0500

    feat: add Implement in-memory storage for weather forecasts with add and delete functionality

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,22 +11,49 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private static List<WeatherForecast> _weatherForecasts = new List<WeatherForecast>();
+
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        if (_weatherForecasts.Count == 0 || !_weatherForecasts.Any())
+        {
+            _weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return _weatherForecasts;
     }
+
+
+    [HttpPost(Name = "AddWeatherForecast")]
+    public IActionResult Post([FromBody] WeatherForecast weatherForecast)
+    {
+        _weatherForecasts.Add(weatherForecast);
+        return CreatedAtRoute("GetWeatherForecast", new { }, weatherForecast);
+    }
+
+    [HttpDelete("{position}", Name = "DeleteWeatherForecast")]
+    public IActionResult Delete(int position)
+    {
+        if (position < 0 || position >= _weatherForecasts.Count)
+        {
+            return NotFound();
+        }
+
+        _weatherForecasts.RemoveAt(position);
+        return NoContent();
+    }
+    
 }
